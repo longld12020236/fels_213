23973786aa58ca8fa810ae8dfc2fda082138230f
Ly Duc Long <longlyduc@gmail.com>
2016-12-01 09:37:03 +0000

    Register

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root  "static_pages#home"
   get "contact" =>  "static_pages#contact"
   get "login" =>  "sessions#new"
-  post "login"  =>  "sessions#create"
+  post "login" =>  "sessions#create"
   delete "logout" =>  "sessions#destroy"
+  get "signup" => "users#new"
+
+  resources :users
 end
